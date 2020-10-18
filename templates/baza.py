27ba9d92bcc428290afff28e9e9b32d536944f68
AnnaGottfried <68641327+AnnaGottfried@users.noreply.github.com>
2020-10-18 15:00:33 +0200

    Delete baza.py

--- templates/baza.py
+++
@@ -1,9 +0,0 @@
-class Parent(Base):
-    __tablename__ = 'parent'
-    id = Column(Integer, primary_key=True)
-    children = relationship("Child")
-
-class Child(Base):
-    __tablename__ = 'child'
-    id = Column(Integer, primary_key=True)
-    parent_id = Column(Integer, ForeignKey('parent.id'))
